abdd6e68e7e4065e7882b38f44641f73e65f14a5
Eduardo Nogueira <eduardodudunog@gmail.com>
2025-11-19 11:30:56 -0300

    feat: setup frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/theme";
+
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "HealthFlow",
+  description: "HealthFlow",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={roboto.variable}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
---
+++ frontend/src/theme.ts
@@ -0,0 +1,28 @@
+"use client";
+
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  colorSchemes: { light: true, dark: true },
+  cssVariables: {
+    colorSchemeSelector: "class",
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: {
+          variants: [
+            {
+              props: { severity: "info" },
+              style: {
+                backgroundColor: "#60a5fa",
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
+});
+
+export default theme;
