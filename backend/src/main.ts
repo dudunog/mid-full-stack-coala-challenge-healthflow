f2177b64a88ea02f40998e6eb1cb20d56c06e4f5
Eduardo Nogueira <eduardodudunog@gmail.com>
2025-11-19 11:40:21 -0300

    feat: enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,11 @@ import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor());
 
